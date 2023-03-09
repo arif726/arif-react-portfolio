3040bd45f84f3f3449be906e48c7b6602de68bf1
arif726 <akonarif3@gmail.com>
2023-03-10 00:25:55 +0600

    navbar completed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
+
+    const [nav, setNav] = useState(false);
 
     const links = [
         {
@@ -31,15 +33,35 @@ const NavBar = () => {
           <div>
               <h1 className='font-signature text-5xl ml-2'>Arif</h1>
           </div>
-          <ul className='flex'>
+          <ul className='hidden md:flex'>
             
               {links.map(({id, link}) => (
                   <li
                       key={id}
-                      className='p-4 cursor-pointer font-medium text-gray-500 capitalize hover:scale-105 duration-200'>{link}</li>
+                      className='p-4 cursor-pointer font-medium text-gray-500 capitalize hover:scale-105 duration-200'>{link}
+                  </li>
                 ))}
               
           </ul>
+
+          <div onClick={()=>setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden'>
+              {nav?<FaTimes size={30}/>:<FaBars size={30}/>}
+          </div>
+
+          {nav && (
+              <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
+              
+              {links.map(({id, link}) => (
+                  <li
+                      key={id}
+                      className='capitalize cursor-pointer px-4 py-6 text-4xl'>{link}
+                  </li>
+                ))}
+
+          </ul>
+          )}
+
+
       </div>
   )
 }
